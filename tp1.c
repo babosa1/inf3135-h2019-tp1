9f25a1d978f806618ef49624d130ef1a08630362
Walvens J Gustelia <wawagustavo@hotmail.com>
2019-02-19 15:40:18 -0500

    Rien

--- tp1.c
+++ tp1.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include<ctype.h>
 
 
 void nbrPa(int limite);
 int main()
 {
- 
+ FILE* cp=NULL;
+  FILE* code=NULL;
+char CODE_permanent[12];
   int limite;
   char reponse;
-
   int n, somme_diviseur, diviseur; //int n, somme_diviseur, diviseur; 
+  cp=fopen("cp.txt","r");
+  if(cp!=NULL){
+    do{
+       fgets(CODE_permanent,12,stdin);
+      printf("%c\n",CODE_permanent);
+      printf("%c",getchar());
+    } while (CODE_permanent !=EOF);
+    fclose(cp);
+  }
+    code=fopen("code.txt","w");
+    if(code!= NULL){
   printf("\nEntrez votre nombre:");
-  scanf("%d",&limite);
-
-  
+  while(scanf("%d",&limite));{
     for(n=1; n<=limite; n++)
     {
       somme_diviseur=0;
@@ -20,11 +31,10 @@ int main()
         if(n%diviseur==0)
           somme_diviseur+=diviseur;
         if(somme_diviseur==n)
-      
- 
-        printf("%d est un nombre parfait\n", n);
+             printf("%d est un nombre parfait", n);
+    }
        
-    
+  }
   
  //printf("\nVoulez-vous continuer(o/n)?");
   // scanf("%c",&reponse);
